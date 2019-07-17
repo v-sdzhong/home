815024f23e00761a9f4b584881ca9d1cb9009eea
v-sdzhong <v-sdzhong@isoftstone.com>
2019-07-17 11:28:05 +0800

    first commit

---
+++ demo/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/say")
+    private String say(){
+        System.out.println("你好");
+        return "你好";
+    }
+}
