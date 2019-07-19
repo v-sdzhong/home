84dbb9ace58b267e31d95ae07f87d3bfa5e9de9b
v-sdzhong <v-sdzhong@isoftstone.com>
2019-07-19 15:01:19 +0800

    7.19

---
+++ demo1/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+
+
+    public static void man(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
